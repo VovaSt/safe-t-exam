81cf5a342bf7031d2ba259bbda5ecf4f5a339e17
Vova <volodymyr.stakhov@smartexe.com>
2021-03-10 15:28:19 +0200

    Little fix

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
+export interface TableData {
   name: string;
   type: string;
   ip: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: TableData[] = [
   { name: 'github', type: 'Application', ip: '140.82.121.3' },
   { name: 'sub-network A', type: 'Subnetwork', ip: '10.10.10.1-10.10.10.250' },
 ];
+
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
@@ -22,7 +23,5 @@ export class ServicesComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
